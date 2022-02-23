31b964ea300773267a7a62a985a749789aeda485
timo.heiten <timoheiten@googlemail.com>
2022-02-23 21:00:53 +0100

    added initial structure

---
+++ tests/heitech.consoleXt.example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace heitech.consoleXt.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
